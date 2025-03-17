5cdb39aeb773ae50fdde5a441ba79c14b981212f
spyflow <133718732+spyflow@users.noreply.github.com>
2025-03-17 12:49:47 -0300

    Actualizar index.php

--- api/peliculas/buscar/index.php
+++ api/peliculas/buscar/index.php
@@ -29,7 +29,7 @@ function getWebContent($url) {
         'http' => [
             'method' => 'GET',
             'timeout' => 10,
-            'header' => "User-Agent: Mozilla/5.0\r\n"
+            'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36\r\n"
         ]
     ]);
 
